84b91ddd7998a58d1c21e9f3837c59c0aa39e3be
GabbeG37 <gadw@kth.se>
2024-09-05 09:53:42 +0200

    Add files via upload

---
+++ Nianstabellbby.py
@@ -0,0 +1,12 @@
+def main():
+    # Skriver ut en multiplikationstabell
+    n = 9
+    print()
+    print("_______________________")
+    print("Multiplikationstabell för", n)
+    print("_______________________")
+    for i in range(1, 11):
+        print(i*n)
+    print()
+
+main()
